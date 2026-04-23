676cce7951bdc64b6c5805873e37dd65a8a2bba6
Aarav Kapoor <aaravkapoor1221@gmail.com>
2026-04-23 11:16:20 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import path from "path";
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+
+import { functions, inngest} from './config/inngest.js';
+
 
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
@@ -9,7 +13,10 @@ const app = express();
 
 const __dirname = path.resolve();
 
+app.use(express.json())
 app.use(clerkMiddleware()); // adds auth object under request 
+
+app.use("/api/inngest", serve({client:inngest, functions }));
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({ status: "Success" });
